36195dcf50cad379c2fbca42328d3e2bee7e19f0
Rutenis Raila <rutenisraila@hotmail.com>
2020-01-18 16:48:22 +0000

    todo-list & initial tests

---
+++ src/components/Todos.js
@@ -0,0 +1,90 @@
+import React, { useState, useRef } from 'react';
+
+const Todos = () => {
+  const [todos, setTodos] = useState([
+    { id: 0, item: 'Fix bugs' },
+    { id: 1, item: 'Take out the trash' }
+  ]);
+  const todoRef = useRef();
+  const removeTodo = id => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+  const addTodo = data => {
+    let id = todos.length + 1;
+    setTodos([
+      ...todos,
+      {
+        id,
+        item: data
+      }
+    ]);
+  };
+  const handleNewTodo = e => {
+    e.preventDefault();
+    const item = todoRef.current;
+    addTodo(item.value);
+    item.value = '';
+  };
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 mt-4">
+          <h2>Add Todo</h2>
+        </div>
+      </div>
+      <form>
+        <div className="row">
+          <div className="col-md-6">
+            <input
+              type="text"
+              autoFocus
+              ref={todoRef}
+              placeholder="Enter a task"
+              className="form-control"
+              data-testid="input"
+            />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-6 mt-2">
+            <button
+              type="submit"
+              onClick={handleNewTodo}
+              className="btn btn-primary"
+            >
+              Add Task
+            </button>
+          </div>
+        </div>
+      </form>
+      <div className="row todo-list">
+        <div className="col-md-6 mt-4">
+          <h3>Tasks</h3>
+          {!todos.length ? (
+            <div className="no-task">No tasks!</div>
+          ) : (
+            <ul data-testid="todos">
+              {todos.map(todo => {
+                return (
+                  <li key={todo.id}>
+                    <div>
+                      <span>{todo.item}</span>
+                      <button
+                        className="btn btn-danger ml-4"
+                        data-testid="delete-button"
+                        onClick={() => removeTodo(todo.id)}
+                      >
+                        X
+                      </button>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Todos;
---
+++ src/components/Todos.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import Todos from './Todos';
+
+describe('Todos', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(<Todos />);
+  });
+
+  it('should render Todos component', () => {
+    shallow(<Todos />);
+  });
+
+  it('should display 2 initial to-dos', () => {
+    expect(wrapper.find('li')).toHaveLength(2);
+  });
+
+  it('should a new item by typing item name and clicking button', () => {
+    wrapper.find('input').instance().value = 'Visit a drug store';
+    expect(wrapper.find('input').instance().value).toEqual(
+      'Visit a drug store'
+    );
+    wrapper.find('[type="submit"]').simulate('click');
+    expect(wrapper.find('li')).toHaveLength(3);
+    expect(
+      wrapper
+        .find('li div span')
+        .last()
+        .text()
+    ).toEqual('Visit a drug store');
+  });
+
+  it('should remove item by clicking remove button', () => {
+    wrapper
+      .find('li button')
+      .first()
+      .simulate('click');
+    expect(wrapper.find('li')).toHaveLength(1);
+    expect(wrapper.find('li span').map(item => item.text())).toEqual([
+      'Take out the trash'
+    ]);
+  });
+});
